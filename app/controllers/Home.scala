cec8e857b76cf4d3cfa2cef43746546d487f17af
Suo Qin <kpsesuo@gmail.com>
2015-01-25 22:20:25 +0800

    new about Ninterior page

---
+++ app/controllers/Home.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.mvc._
+
+object Home extends Controller {
+
+  def aboutRed = Action {
+    Ok(views.html.aboutRed("Your new application is ready."))
+  }
+
+  def aboutN = Action {
+    Ok(views.html.aboutN("Your new application is ready."))
+  }
+
+}
