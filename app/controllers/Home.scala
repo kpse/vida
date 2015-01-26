465e4398eb0d65e2557258d22a732cdedaf5d9a5
Suo Qin <kpsesuo@gmail.com>
2015-01-26 21:27:26 +0800

    add team, contact and work pages

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -5,11 +5,27 @@ import play.api.mvc._
 object Home extends Controller {
 
   def aboutRed = Action {
-    Ok(views.html.aboutRed("Your new application is ready."))
+    Ok(views.html.aboutRed("About RED - RED|N"))
   }
 
   def aboutN = Action {
     Ok(views.html.aboutN("About N interior - RED|N"))
+  }
+
+  def aboutContact = Action {
+    Ok(views.html.aboutContact("About Contact - RED|N"))
+  }
+
+  def aboutWork = Action {
+    Ok(views.html.aboutWork("About Work - RED|N"))
+  }
+
+  def aboutTeam = Action {
+    Ok(views.html.aboutTeam("About Team - RED|N"))
+  }
+
+  def main = Action {
+    Ok(views.html.aboutMain("About - RED|N"))
   }
 
 }
